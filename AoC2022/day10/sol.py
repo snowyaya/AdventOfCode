4d6aecdac27222d28cd73b580f8ad67f23bde880
snowyaya <yayingliang@gmail.com>
2022-12-10 13:08:44 -0500

    Complete part 2

--- AoC2022/day10/sol.py
+++ AoC2022/day10/sol.py
@@ -2,7 +2,7 @@ import sys
 import os 
 import re
 
-with open(os.path.join(sys.path[0], "input0.txt"), "r") as f:
+with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
     lines = f.read().strip().splitlines()
 
 '''
@@ -18,52 +18,29 @@ cycles = [20, 60, 100, 140, 180, 220]
 visited = set()
 cycles_and_values = []
 
-CRT_num = 0 # resets to 0 after 40
-sprite_pos= 0
-image = [["."] * 40 for i in range(6)]
-# print(image)
-col = 0
-hash_loc = set()
 
 for line in lines:
     if line == "noop":  
-        # print("---Begin noop cycle", cycle_num, "register value is", register_value)      
+        # print("---Begin noop cycle", cycle_num, "register value is", register_value) 
         cycles_and_values.append((cycle_num, register_value))
-        cycle_num += 1
-        cycles_and_values.append((cycle_num, register_value))
-        # print("---After noop", cycle_num, "register value is", register_value)
         
-        # CRT_num += 1
-        # if CRT_num == 40:
-        #     CRT_num = 0
-        #     col += 1
+        cycle_num += 1
+        # cycles_and_values.append((cycle_num, register_value))
+        # print("---After noop", cycle_num, "register value is", register_value)
+
     elif line.startswith("addx"):
+        cycles_and_values.append((cycle_num, register_value))
         # print("---Begin addx cycle", cycle_num, "register value is", register_value)
         cycle_num += 1
         cycles_and_values.append((cycle_num, register_value))
         curr_value = re.findall(r'-?\d+', line)[0]
         # print("---During addx cycle", cycle_num, "register value is", register_value)
         
-        # sprite_pos = register_value
-        # print("CRT_num", CRT_num, "sprite_pos", sprite_pos, "col", col)
-        # if CRT_num == sprite_pos or CRT_num == sprite_pos + 1 or CRT_num == sprite_pos - 1:
-        #     image[CRT_num][col] = "#"
-        #     hash_loc.add((CRT_num, col))
-        
-        # CRT_num += 1
-        # if CRT_num == 40:
-        #     CRT_num = 0
-        #     col += 1
-        
         cycle_num += 1
         register_value += int(curr_value)
         # print("~~~After addx cycle", cycle_num, "register value is", register_value)
-        cycles_and_values.append((cycle_num, register_value))
-        
-        # CRT_num += 1
-        # if CRT_num == 40:
-        #     CRT_num = 0
-        #     col += 1
+        # cycles_and_values.append((cycle_num, register_value))
+      
 # print(cycles_and_values)
 
 sum_ = 0
@@ -75,5 +52,19 @@ for cycle in cycles:
             # print("Cycle", cycle, "register value is", cycle_and_value[1])
 
 print("Part 1:", sum_)
-# print("Part 2:", image)
+
+sprites = []
+for cycle, value in cycles_and_values:
+    sprites.append([value-1, value, value+1])
+
+print("Part 2:")
+for i in range(0, 6):
+    render = []
+    for j in range(0, 40):
+        if j in sprites[j + (40 * i)]:
+            # print("j", j, "sprites", sprites[j + (40 * i)])
+            render.append("#")
+        else:
+            render.append(".")
+    print(render)
 
