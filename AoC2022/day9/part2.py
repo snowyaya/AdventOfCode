dda29ede772562dd5eb2bfaa191f3beaac62e319
snowyaya <yayingliang@gmail.com>
2022-12-09 20:03:10 -0500

    Refactor part2

--- AoC2022/day9/part2.py
+++ AoC2022/day9/part2.py
@@ -1,33 +1,34 @@
 import sys 
 import os 
 
-with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+with open(os.path.join(sys.path[0], "input0.txt"), "r") as f:
     lines = f.read().strip().splitlines()
 
 def make_moves(moves, rope_len):
-    xs = [0] * rope_len
-    ys = [0] * rope_len
+    rows = [0] * rope_len
+    cols = [0] * rope_len
     
-    visited = { xs[-1], ys[-1] }
+    visited = { rows[-1], cols[-1] }
     
-    for (mx, my), distance in moves:
+    for (x, y), distance in moves:
         for _ in range(distance):
-            xs[0] += mx
-            ys[0] += my
+            rows[0] += x
+            cols[0] += y
             
             for i in range(rope_len - 1):
-                dx = xs[i+1] - xs[i]
-                dy = ys[i+1] - ys[i]
+                dx = rows[i+1] - rows[i]
+                dy = cols[i+1] - cols[i]
                 if abs(dx) == 2 or abs(dy) == 2:
-                    xs[i+1] = xs[i] + int(dx / 2)
-                    ys[i+1] = ys[i] + int(dy / 2)
-            visited.add((xs[-1], ys[-1]))
+                    rows[i+1] = rows[i] + int(dx / 2)
+                    cols[i+1] = cols[i] + int(dy / 2)
+            visited.add((rows[-1], cols[-1]))
     return len(visited)
 
 dirs = {'L': (-1, 0), 'R': (1, 0), 'U': (0, 1), 'D': (0, -1)}
 moves = [(dirs[line[0]], int(line[1:])) for line in lines]
+print(moves)
 
-print("Part 1:", make_moves(moves, 2) - 1)
+# print("Part 1:", make_moves(moves, 2) - 1)
 print("Part 2:", make_moves(moves, 10) - 1)
 
                     
